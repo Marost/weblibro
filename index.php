ae9822ec88daf77764e7827e8c2d5fef8cad5af6
Marco Lopez <mlopez@marostdevelopers.com>
2015-01-14 17:31:57 -0500

    libro de uchuraccay

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<!--[if IE 7]><html lang="es" class="ie ie7"><![endif]-->
+<!--[if IE 8]><html lang="es" class="ie ie8"><![endif]-->
+<!--[if IE 9]><html lang="es" class="ie ie9"><![endif]-->
+<!--[else]><!-->
+<html lang='es' itemscope itemtype="http://schema.org/Product">
+<!--<![endif]-->
+
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+  <title>Uchuraccay - El pueblo donde morían los que llegaban a pie</title>
+    
+  <link href="css/font.css?update=<?php echo date("YmdHi") ?>" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="css/estilos.css?update=<?php echo date("YmdHi") ?>" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="css/owl.carousel.css?update=<?php echo date("YmdHi") ?>" media="screen, projection" rel="stylesheet" type="text/css" />
+
+  <script src="js/custom.js?update=<?php echo date("YmdHi") ?>" type="text/javascript"></script>
+
+  <!-- FONT AWESOME -->
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+  <!-- FANCYBOX -->
+  <script type="text/javascript" src="libs/fancybox/jquery-1.10.1.min.js"></script>
+  <script type="text/javascript" src="libs/fancybox/jquery.fancybox.js?v=2.1.5"></script>
+  <link rel="stylesheet" type="text/css" href="libs/fancybox/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+  <script type="text/javascript">
+    var jPopUp = jQuery.noConflict();
+    jPopUp(document).ready(function() {
+      
+      jPopUp("#comprar").click(function() {
+        jPopUp.fancybox.open({
+          href : 'iframe.html',
+          type : 'iframe',
+          padding : 5
+        });
+      });
+
+    });
+  </script>
+  <style type="text/css"> .fancybox-custom .fancybox-skin { box-shadow: 0 0 50px #222; } </style>
+
+  <!-- Allows CSS3 things like :not to work on < IE9 -->
+  <!--[if lt IE 9]>
+  <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+  <![endif]-->
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+  
+</head>
+<body class="es-es">
+
+
+<div class="taco-main m-lightbox">
+  <div class="taco-main--header">
+    <div class="taco-main--header--content">
+      <div class="taco-main--header--logo taco-main--header--logo__original"></div>
+      <div class="taco-main--header--text">
+        <div class="taco-main--header--text--wrapper">
+          <h3 class="taco-main--header--title">Más de 90 testimonios y documentos inéditos.</h3>
+          <h3 class="taco-main--header--title">Dos años de investigación en las alturas de Ayacucho y Lima.</h3>
+          <h3 class="taco-main--header--title">Reveladores hallazgos para una matanza aún sin resolver.</h3>
+          <h3 class="taco-main--header--title">¿Quién mató a los periodistas, en Uchuraccay, el 26 de enero de 1,983?</h3>          
+        </div>
+      </div>
+    </div>
+    <div class="taco-main--header--image">
+      <img alt="Header-book" src="imagenes/header-book.jpg" />
+    </div>
+  </div>
+
+  <div class="taco-main--header-links">
+    <div class="taco-main--icons">
+      <div class="taco-main--icons--items">
+        <a class="taco-main--icons--item taco-main--icons--item__materials" href="#magic">
+          <span class="taco-main--icons--item--content">
+            <span class="taco-main--icons--item--icon"></span>
+            <span class="taco-main--icons--item--text">Book Trailer</span>
+          </span>
+        </a>
+        <a class="taco-main--icons--item taco-main--icons--item__sizes" href="#sizes">
+          <span class="taco-main--icons--item--content">
+            <span class="taco-main--icons--item--icon"></span>
+            <span class="taco-main--icons--item--text">Venta</span>
+          </span>
+        </a>
+        <a class="taco-main--icons--item taco-main--icons--item__shipping" href="#shipping">
+          <span class="taco-main--icons--item--content">
+            <span class="taco-main--icons--item--icon"></span>
+            <span class="taco-main--icons--item--text">Distribución</span>
+          </span>
+        </a>
+        <a class="taco-main--icons--item taco-main--icons--item__magic" href="#footer-quotes">
+          <span class="taco-main--icons--item--content">
+            <span class="taco-main--icons--item--icon"></span>
+            <span class="taco-main--icons--item--text">La Historia</span>
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="taco-main--drop-menu">
+    <div class="taco-main--drop-menu--container">
+      <div class="taco-main--drop-menu--outer-wrapper">
+        <div class="taco-main--icons">
+          <div class="taco-main--icons--items">
+            <a class="taco-main--icons--item taco-main--icons--item__materials" href="#magic">
+              <span class="taco-main--icons--item--content">
+                <span class="taco-main--icons--item--icon"></span>
+                <span class="taco-main--icons--item--text">Book Trailer</span>
+              </span>
+            </a>
+            <a class="taco-main--icons--item taco-main--icons--item__sizes" href="#sizes">
+              <span class="taco-main--icons--item--content">
+                <span class="taco-main--icons--item--icon"></span>
+                <span class="taco-main--icons--item--text">Venta</span>
+              </span>
+            </a>
+            <a class="taco-main--icons--item taco-main--icons--item__shipping" href="#shipping">
+              <span class="taco-main--icons--item--content">
+                <span class="taco-main--icons--item--icon"></span>
+                <span class="taco-main--icons--item--text">Distribución</span>
+              </span>
+            </a>
+            <a class="taco-main--icons--item taco-main--icons--item__magic" href="#footer-quotes">
+              <span class="taco-main--icons--item--content">
+                <span class="taco-main--icons--item--icon"></span>
+                <span class="taco-main--icons--item--text">La Historia</span>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="taco-main--content-top">
+    <a href="#magic" class="taco-main--content-top--learn-more">Más información</a>
+  </div>
+
+  <div class="taco-main--content">
+    <div class="taco-main--content--section taco-main--content--section__magic" id="magic">
+      <div class="taco-main--content--title">
+        <div class="taco-main--header--logo taco-main--header--logo__original"></div>
+      </div>
+      <div class="taco-main--content--subtitle">
+        <iframe width="900" height="450" src="//www.youtube.com/embed/feWHlWL4i6Q" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div class="taco-main--content--subtitle">
+        <p>Es el resultado de una investigación periodística que permitió encontrar documentos y testigos de aquella tarde mortal y revelar cómo y en qué contexto se produjeron los hechos. </p>
+        <p>La publicación reconstruye en un formato de gran reportaje los últimos días de los 8 hombres de prensa y el guía, hasta el momento mismo de su muerte aportando al caso, aún abierto, hallazgos sorprendentes que configuran una nueva realidad y generarán una inevitable polémica.</p>
+      </div>
+    </div>
+
+    <div class="taco-main--content--section taco-main--content--section__sizes" id="sizes">
+      <div class="taco-main--content--title">Reserva y adquiere un ejemplar</div>
+      <div class="taco-main--content--subtitle">208 páginas, fotografías y documentos en un formato de colección.</div>
+
+      <div class="taco-main--content--books">          
+
+          <div class="taco-main--content--books--book taco-main--content--books--book__large">
+            <div class="taco-main--content--books--book--prices">
+                <div class="taco-main--content--books--book--prices--price">
+                  <span class="m-dollar">S/.</span>50.00
+                </div>
+                <a href="javascript:;" id="comprar" class="taco-main--button taco-main--button__start taco-main--button__primary taco-main--button__big taco-main--header--buttons--start">Comprar</a>
+                <div class="logo-libro"><img src="imagenes/logo-g7.png" alt="G7 Consultores"></div>
+            </div>            
+          </div>
+
+        <div class="taco-main--content--books--shelf"></div>
+      </div>
+    </div>
+
+    <div class="taco-main--content--section taco-main--content--section__shipping" id="shipping">
+      <div class="taco-main--content--text domestic-version">
+        <div class="taco-main--content--title">Reciba su libro en la comodidad de su casa u oficina</div>
+        <div class="taco-main--content--subtitle">Envío a domicilio con recarga: S/. 8.00 para Lima Metropolitana.</div>
+        <div class="taco-main--content--subtitle">Tarifa variable según destino a nivel nacional e internacional.</div>
+        <div class="taco-main--content--subtitle">Consultas: <a href="https://www.facebook.com/uchuraccay" target="_blank"><i class="fa fa-facebook-square fa-lg"></i> https://www.facebook.com/uchuraccay</a></div>
+      </div>
+      <div class="taco-main--content--image">
+        <div class="taco-main--content--image--cityscape"></div>
+        <div class="taco-main--content--image--wheels"></div>
+        <div class="taco-main--content--image--truck"></div>
+        <div class="taco-main--content--image--road"></div>
+      </div>
+    </div>
+
+    <div class="taco-main--content--section taco-main--content--section__layflat" id="layflat">
+      <div class="taco-main--content--image-wrapper">
+        <div class="taco-main--content--image-tiles"></div>
+        <div class="taco-main--content--image">
+          <img alt="Layflat Press" src="imagenes/layflat-detail.jpg" />
+        </div>
+      </div>
+    </div>
+
+    <div class="taco-main--content--footer">
+      <svg class="taco-main--content--footer--shadow"></svg>
+      <div class="taco-main--content--footer--content">
+        <div class="taco-main--content--footer--quotes" id="footer-quotes">
+          <div class="taco-main--content--title">
+            <div class="taco-main--header--logo taco-main--header--logo__original blanco"></div>
+          </div>
+          <div class="taco-main--content--image"></div>
+          <div class="taco-main--content--footer--quotes--carousel">
+            <div id="footer-quotes-carousel">
+
+                <div class="taco-main--content--footer--quotes--carousel--quote">
+                  <p class="quote">El 26 de enero de 1983 ocurrió un trágico hecho que conmocionó al mundo entero: en Uchuraccay, un remoto paraje de la sierra alta de Ayacucho, Perú, ocho periodistas y un guía fueron brutalmente asesinados por campesinos de esa comunidad andina. Los hombres de prensa cumplían la misión de investigar la muerte de siete integrantes de Sendero Luminoso, un grupo maoísta que tres años antes se había levantado en armas contra el Estado peruano.</p>
+                </div>
+                <div class="taco-main--content--footer--quotes--carousel--quote">
+                  <p class="quote">Mucho se ha escrito y comentado desde entonces sobre la masacre de Uchuraccay. A veces, como es comprensible, con una gran carga emotiva o ideológica, casi siempre con un subjetivismo que ha terminado siendo muy perjudicial porque ha  enturbiado los reiterados intentos por esclarecer aquellos hechos dramáticos que dejaron una cicatriz profunda en el periodismo peruano.</p>
+                </div>
+                <div class="taco-main--content--footer--quotes--carousel--quote">
+                  <p class="quote">Durante más de tres décadas persistieron las historias contradictorias, pese a que el múltiple crimen se investigó hasta en tres distintas oportunidades. En primer lugar, fue una comisión especial encabezada por el hoy Premio Nobel, el escritor Mario Vargas Llosa, la que elaboró un informe que en su momento generó muchas expectativas; luego el Poder Judicial que procesó a diecisiete comuneros, pero solo envió a tres de ellos a la cárcel porque los demás nunca fueron capturados por la policía. Y, finalmente, una Comisión de la Verdad y Reconciliación (CVR) dedicó al caso un capítulo íntegro de su voluminoso trabajo sobre los años de la violencia en el Perú.</p>
+                </div>
+                <div class="taco-main--content--footer--quotes--carousel--quote">
+                  <p class="quote">Con los años la prensa perdió el interés por investigar y cada aniversario se limitó a publicar notas conmemorativas exhortando conocer la verdad, pero extraviando en el tiempo una cadena de sucesos fundamentales.</p>
+                </div>                
+            </div>
+            <svg class="taco-main--content--footer--quotes--shadow-left"></svg>
+            <svg class="taco-main--content--footer--quotes--shadow-right"></svg>
+          </div>
+        </div>
+        <div class="taco-main--content--footer--separator">
+          G7 EDITORES es una división de G7 Consultores S.A.C. Copyright 2015 <br> <a id="footer_enlace" href="http://g7consultores.com" target="_blank">www.g7consultores.com</a>
+        </div>        
+    </div>
+  </div>
+
+  
+  <script type="text/javascript">
+//<![CDATA[
+
+      $(function() {
+        var elm = $("#footer-quotes-carousel");
+        elm.owlCarousel({
+          center: true,
+          items: 4,
+          autoWidth: true,
+          margin: 100,
+          autoPlay: true
+        });
+
+        var carousel = elm.data('owlCarousel');
+
+        $(".owl-stage-outer").click(function(e){
+          var overallWidth = $(e.currentTarget).width();
+
+          var relativeXPosition = (e.pageX - this.offsetLeft); //offset -> method allows you to retrieve the current position of an element 'relative' to the document
+          var percentageX = relativeXPosition / overallWidth;
+
+          if (percentageX < 0.5) {
+            carousel.prev();
+          } else {
+            carousel.next();
+          }
+        });
+      });
+
+//]]>
+</script>  
+<script type="text/javascript">
+//<![CDATA[
+window.Mixbook.Cells.TacoMain.Main()
+//]]>
+</script>
+  
+  
+  <script type="text/javascript">
+//<![CDATA[
+window.Mixbook.Modules.Lightbox(null, {overlay: true})
+//]]>
+</script>
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-20229980-28', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
