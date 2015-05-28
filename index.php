28c21e1323d5e66e93cff5f5b0d504e8cc1fdcca
Marco Lopez <mlopez@marostdevelopers.com>
2015-05-28 11:28:49 -0500

    facebook

--- index.php
+++ index.php
@@ -47,6 +47,26 @@
   <![endif]-->
   
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+
+  <script>
+    (function() {
+    var _fbq = window._fbq || (window._fbq = []);
+    if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+    }
+    _fbq.push(['addPixelId', '594376604040375']);
+    })();
+    window._fbq = window._fbq || [];
+    window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript>
+  <img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=594376604040375&amp;ev=PixelInitialized" />
+</noscript>
   
 </head>
 <body class="es-es">
